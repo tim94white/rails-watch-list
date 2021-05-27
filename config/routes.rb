d4cd001c9f172b2184f238dc847e29b20c81d617
Tim White <tim94white@gmail.com>
2021-05-27 16:59:30 +0100

    bookmark functionality working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[index new create show]
-
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: %i[new create]
+  end 
+  resources :bookmarks, only: %i[destroy]
 end
