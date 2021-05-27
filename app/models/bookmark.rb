7151f953ee29f9df97c93b9cede22da1ee352267
Tim White <tim94white@gmail.com>
2021-05-27 20:37:35 +0100

    added styling to buttons

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum:6 }
-  validates :movie, uniqueness: { scope: :list }
+  validates :movie, presence: true
 end
+
+
+# , uniqueness: { scope: :list }
